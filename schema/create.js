5f1aa150a0043a9c0fbd5a405222588f6dafe990
Justin Merz <jrmerz@gmail.com>
2015-04-22 22:03:48 -0400

    start of working in dataset level attributes and new theme

--- schema/create.js
+++ schema/create.js
@@ -1,6 +1,19 @@
 var csv = require("fast-csv");
 var fs = require("fs");
 
+var column = {
+  category : 0,
+  l1 : 1,
+  l2 : 2,
+  inputType : 3,
+  allowOther : 4,
+  datasetLevel : 5,
+  units : 6,
+  comment : 7,
+  controlledVocab : 8,
+  description : 9
+}
+
 
 load(function(data){
     create(data);
@@ -10,7 +23,7 @@ load(function(data){
 function load(callback) {
     var data = [];
     csv
-        .fromPath(__dirname+'/data/schema.csv')
+        .fromPath(__dirname+'/data/schema_v1.csv')
         .on('data', function(row){
             data.push(row);
         })
@@ -28,26 +41,27 @@ function create(data) {
 }
 
 function readCategories(row, data, schema, callback) {
-    var catName = data[row][0];
+    var catName = data[row][column.category];
     schema[catName] = [];
 
     row++;
-    while( data[row][0] == '' ) {
+    while( data[row][column.category] == '' ) {
         var level = 1;
-        var name = data[row][1];
-        
+        var name = data[row][column.l1];
+
         // check for level 2 data
         if( name == '' ) {
             level = 2;
-            name = data[row][2];
+            name = data[row][column.l2];
         }
 
-        var input = data[row][3];
-        var units = data[row][4];
+        var input = data[row][column.inputType];
+        var units = data[row][column.units];
+        var datasetLevel = data[row][column.datasetLevel] == 'TRUE' ? true : false;
 
         var vocabulary = null;
-        if( data[row][6].indexOf(',') > -1 ) {
-            vocabulary = data[row][6].split(',');
+        if( data[row][column.controlledVocab].indexOf(',') > -1 ) {
+            vocabulary = data[row][column.controlledVocab].split(',');
             for( var i = 0; i < vocabulary.length; i++ ) {
                 vocabulary[i] = prepVocab(vocabulary[i])
             }
@@ -60,7 +74,10 @@ function readCategories(row, data, schema, callback) {
                 level : level,
                 input : input,
                 units : units,
-                vocabulary : vocabulary
+                datasetLevel: datasetLevel,
+                vocabulary : vocabulary,
+                description : data[row][column.description] || '',
+                allowOther : data[row][column.allowOther] == 'TRUE' ? true : false
             });
         }
 
@@ -73,6 +90,8 @@ function readCategories(row, data, schema, callback) {
 }
 
 function write(schema) {
+    if( schema[""] ) delete schema[""];
+
     fs.writeFileSync(__dirname+'/../app/schema.json', JSON.stringify(schema, '', '  '));
     console.log('done.');
 }
